c72f9ed74863a2f638a0347942377ac2995a7d2a
Crowdbotics <team@crowdbotics.com>
2024-01-04 23:02:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ent-learning-altria-45284",
+  url: "https://ent-learning-altria-45284.botics.co",
+  api: "https://ent-learning-altria-45284.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
